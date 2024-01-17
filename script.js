7406153349c1506d94f11e68bc4819d0b249ecde
Dana Alsani <80426346+notdana@users.noreply.github.com>
2024-01-17 10:30:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add event listener to each "Toggle Details" button
-    var toggleButtons = document.querySelectorAll('.toggle-details');
-    toggleButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            // Toggle visibility of the next sibling element with class "details"
-            var details = this.nextElementSibling;
-            details.classList.toggle('visible');
-        });
-    });
-});
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
